fb1092427ba7c64c478baf7e0fae5ad43baa9855
Егор Хохлов <yegorkhohlov@gmail.com>
2018-05-27 20:20:15 +0300

    Added multimedia timer

--- TimerProject/Main.cpp
+++ TimerProject/Main.cpp
@@ -1,13 +1,16 @@
+#pragma comment (lib, "Winmm.lib")
 #include <iostream>
 #include <Windows.h>
 #include <string>
 #include <intrin.h>
 #include <conio.h>
+#include <fstream>
+#include <time.h>
 using namespace std;
-
 #define title "Хохлов Е.С. КИз-16"
-int temp = 500;
 
+clock_t start, endd;
+int temp = 500;
 
 HDC hDC;
 HWND hWnd;
@@ -17,20 +20,15 @@ void GetHwnd(){ hWnd = GetConsoleWindow(); }
 void GetHdc(){ hDC = GetDC(hWnd); }
 void GetHandle() { Handle = GetStdHandle(STD_OUTPUT_HANDLE); }
 
-
-void CALLBACK Print(HWND hwnd, UINT uMsg, UINT timerId, DWORD dwTime)
-{
-	cout << "A" << endl;
-}
-
 void SetPosition(int x, int y)
 {
+	endd = start;
+	start = clock();
 	COORD coord;
 	coord.X = x;
 	coord.Y = y;
 	SetConsoleCursorPosition(Handle, coord);
 }
-
 struct cpuid_regs
 {
 	DWORD   Eax;
@@ -76,6 +74,51 @@ string GetCpuVendorString()
 	}
 	return VendorString;
 }
+void ProcessorName()
+{
+	string name = GetCpuVendorString();
+	cout << endl << endl << endl << endl << endl << endl;
+	cout << "Название процессора:" << endl;
+	cout << name << endl;
+}
+
+void CALLBACK Print(HWND hwnd, UINT uMsg, UINT timerId, DWORD dwTime)
+{
+	cout << "A";
+}
+
+int col = 0;
+int x = 21;
+int y = 6;
+
+void CALLBACK PrintMultimedia(UINT uTimerID, UINT uMsg,
+	DWORD_PTR dwUser, DWORD_PTR dw1, DWORD_PTR dw2)
+{
+
+		SetPosition(x, y);
+		cout << "A";
+		if (col < 3 || col > 6)
+		{
+			y += 1;
+		}
+		if (col >= 3 && col < 6)
+		{
+			x += 2;
+		}
+		if (col == 6)
+		{
+			y -= 1;
+			x += 2;
+		}
+		
+		if (col == 11)
+		{
+			timeKillEvent(uTimerID);
+			ProcessorName();
+		}
+		col++;
+}
+
 
 
 void SetInterval()
@@ -101,6 +144,41 @@ void SetInterval()
 	system("cls");
 }
 
+
+
+void WriteFile(int col, double *mas)
+{
+	ofstream f;
+	string name;
+	if (temp == 165) { name = "Info165.txt"; }
+	if (temp == 500) { name = "Info500.txt"; }
+	if (temp == 1065) { name = "Info1065.txt"; }
+	f.open(name);
+
+	for (int i = 1; i < col; i++)
+	{
+		f << i << ") " << mas[i] << endl;
+	}
+
+	f.close();
+
+}
+
+
+UINT uTimerID;
+//void Start()
+//{
+//	uTimerID = timeSetEvent(temp, temp, PrintMultimedia, 0, TIME_PERIODIC);
+//	timeKillEvent(uTimerID);
+//}
+
+//void Kill()
+//{
+//	if(col==11)
+//	
+//}
+
+
 int main()
 {
 	SetConsoleOutputCP(1251);
@@ -113,48 +191,59 @@ int main()
 	MoveWindow(hWnd, 300, 300, 400, 400, TRUE);
 
 	MSG msg;
-	int x = 21;
-	int y = 6;
-	bool down = false;
-	bool right = false;
-	bool up = false;
+	char c;
 	unsigned count = 0;
-	
+	double *real = new double[11];
+	SetConsoleTextAttribute(Handle, FOREGROUND_GREEN );
+
+
+	cout << "Выберете тип таймера" << endl;
+	cout << "1)Таймер пользователя" << endl;
+	cout << "2)Мультимедийный таймер" << endl;
+	c = _getch();
+	system("cls");
 
 	SetInterval();
 
-	SetTimer(NULL, 0, temp, (TIMERPROC)&Print);
-	SetConsoleTextAttribute(Handle, FOREGROUND_GREEN );
-	while (GetMessage(&msg, NULL, 0, 0))
+	if (c == '1')
 	{
-		SetPosition(x, y);
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		SetTimer(NULL, 0, temp, (TIMERPROC)&Print);
+		while (GetMessage(&msg, NULL, 0, 0))
+		{
 
-		if (count < 3 || count > 6)
-		{
-			y += 1;
-		}
-		if (count >= 3 && count < 6)
-		{
-			x += 2;
-		}
-		if (count == 6)
-		{
-			y -= 1;
-			x += 2;
-		}
-		if (count == 11)
-			break;
+			SetPosition(x, y);
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
 
-		count++; 
+			if (count < 3 || count > 6)
+			{
+				y += 1;
+			}
+			if (count >= 3 && count < 6)
+			{
+				x += 2;
+			}
+			if (count == 6)
+			{
+				y -= 1;
+				x += 2;
+			}
+			if (count == 11)
+				break;
+
+			real[count] = (double)(start - endd);
+			count++;
+		}	
+	ProcessorName();
+	WriteFile(count, real);
 	}
 
-	string name = GetCpuVendorString();
-	cout << endl << endl << endl << endl << endl << endl;
-	cout << "Название процессора:" << endl;
-	cout << name << endl;
-	_getch();
+	else
+	{
+		uTimerID = timeSetEvent(temp, temp, PrintMultimedia, 0, TIME_PERIODIC);
+	}
+
+	system("pause");
 	return 0;
 }
 
